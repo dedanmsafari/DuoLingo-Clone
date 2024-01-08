d2ecf6ff8f6cb198376d5af3518fa24c3bdce58c
dedanmsafari <dedan.msafari@gmail.com>
2024-01-08 19:01:18 +0300

    multiple image question styles

---
+++ src/components/multipleImageQuestions/multipleImageQuestions.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "stretch",
+  },
+  optionsContainer: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignContent: "space-between",
+  },
+});
