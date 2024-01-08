bda794a6e8ff0ad76a46d38a135bc0f429abcc04
dedanmsafari <dedan.msafari@gmail.com>
2024-01-08 18:58:22 +0300

    Added app.styles

---
+++ App.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 30,
+  },
+});
