919b7360b2c770a99ff9143a4d8963e4cc1ba4f5
dedanmsafari <dedan.msafari@gmail.com>
2024-01-08 19:01:55 +0300

    progress styles

---
+++ src/components/Progress/progress.styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  bg: {
+    flex: 1,
+    height: 20,
+    backgroundColor: "lightgrey",
+    borderRadius: 10,
+  },
+  fg: {
+    backgroundColor: "#fac800",
+    flex: 1,
+    borderRadius: 10,
+  },
+  hg: {
+    alignSelf: "center",
+    backgroundColor: "#fad131",
+    height: 5,
+    borderRadius: 10,
+    width: "90%",
+    marginTop: 5,
+  },
+});
