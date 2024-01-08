4e80dfb9f9ff88ce3eebaff3231c71a5d257afd9
dedanmsafari <dedan.msafari@gmail.com>
2024-01-08 19:00:24 +0300

    button styles

---
+++ src/components/Button/button.styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#58cc02",
+    alignSelf: "stretch",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    borderColor: "#ffffff",
+    borderBottomWidth: 5,
+    borderRadius: 5,
+    borderColor: "#57a600",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 20,
+    fontWeight: "bold",
+    borderBottomWidth: 1.5,
+    borderColor: "#ffffff",
+    textTransform: "uppercase",
+  },
+  disabled: {
+    backgroundColor: "lightgrey",
+    borderColor: "grey",
+  },
+});
