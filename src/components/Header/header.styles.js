afec342ab6ce4b3b67e6ff0d30e2b6cc888bea4f
dedanmsafari <dedan.msafari@gmail.com>
2024-01-08 19:00:40 +0300

    header styles

---
+++ src/components/Header/header.styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  Icon: {
+    width: 30,
+    height: 30,
+    marginHorizontal: 5,
+  },
+  lives: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
