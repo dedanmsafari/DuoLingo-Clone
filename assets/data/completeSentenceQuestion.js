c515c16a6e7f57bbf4cb7c1ca573d055553c3cdc
dedanmsafari <dedan.msafari@gmail.com>
2024-01-12 06:09:55 +0300

    complete sentence data

---
+++ assets/data/completeSentenceQuestion.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: "q1",
+    type: "COMPLETE_SENTENCE",
+    text: ["una", "nina,", "una"],
+    answer: "mujer",
+    options: ["yo", "el", "mujer", "la"],
+  },
+  {
+    id: "q2",
+    type: "COMPLETE_SENTENCE",
+    text: "I work as a ",
+    answer: "dev",
+    options: ["teacher", "pilot", "plumber", "dev"],
+  },
+];
