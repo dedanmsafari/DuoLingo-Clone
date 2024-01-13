e6b0a2ea667829c338e3768d830729276fe8881b
dedanmsafari <dedan.msafari@gmail.com>
2024-01-13 20:57:01 +0300

    word option styles

---
+++ src/components/WordOptions/wordOption.styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  selectionOptions: {
+    marginRight: 20,
+    padding: 12,
+    borderWidth: 2,
+    borderBottomWidth: 4,
+    borderRadius: 10,
+    borderColor: "lightgrey",
+  },
+  textAnswer: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+});
